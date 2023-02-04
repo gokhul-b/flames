812fc29af318fc0471d1902fd144c816e1a008ee
Gokhul B <113249307+gokhul-b@users.noreply.github.com>
2023-02-04 15:05:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App addEntry={addEntry} />
+    <App  />
   </React.StrictMode>
 );
 
